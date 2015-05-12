a6e94b5cb17aac6a894b20ce68a6fcc8d0d46478
SelmanColic <selman.colic@yahoo.com>
2015-05-12 12:13:08 +0200

    haris nema pojma
    haris nema pojma
    al fakat nema pojma

---
+++ app/src/main/java/ba/kudsad/kudsad/Midzic.java
@@ -0,0 +1,10 @@
+package ba.kudsad.kudsad;
+
+/**
+ * Created by Selman on 12.5.2015.
+ */
+public class Midzic {
+    public static void main(String[] args) {
+        // nemas ti pojma harise midzicu
+    }
+}
